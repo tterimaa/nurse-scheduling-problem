bdd47304afc7df44a719b07da627291063e735ca
Tarmo Terimaa <tarmo.terimaa@efecte.com>
2023-07-10 23:19:25 +0300

    Refactor customer bookings input from table to simple form fields

--- routes.py
+++ routes.py
@@ -5,33 +5,44 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/echo', methods=['POST'])
+
+@app.route("/echo", methods=["POST"])
 def echo():
     data = request.data
     return data
 
-@app.route('/endpoint', methods=['POST'])
+
+@app.route("/endpoint", methods=["POST"])
 def endpoint():
     data = request.json
-    num_employees = data['num_employees']
-    num_days = data['num_days']
-    num_hours = data['num_hours']
-    customer_bookings_input = data['customer_bookings']
+    num_employees = data["num_employees"]
+    num_days = data["num_days"]
+    num_hours = data["num_hours"]
+    customer_bookings_input = data["customer_bookings"]
     print(customer_bookings_input)
     customer_bookings = list(map(map_function, customer_bookings_input))
     print(customer_bookings)
-    success, res = solve_shift_scheduling(num_employees, num_days, num_hours, customer_bookings)
+    success, res = solve_shift_scheduling(
+        num_employees, num_days, num_hours, customer_bookings
+    )
 
     if success == False:
-        return jsonify({'status': 400, 'message': 'Processing the request took too long: check parameters'})
+        return jsonify(
+            {
+                "status": 400,
+                "message": "Processing the request took too long: check parameters",
+            }
+        )
 
-    return jsonify({'status': 200, 'message': 'OK', 'res': res})
+    return jsonify({"status": 200, "message": "OK", "res": res})
+
 
 def map_function(obj):
-    day = obj['day']
-    hour = obj['hour']
-    bookings = int(obj['bookings'])
+    day = obj["day"]
+    hour = obj["hour"]
+    bookings = obj["bookings"]
     return (day, hour, bookings)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- ui/app.js
+++ ui/app.js
@@ -1,71 +1,100 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
-const tableContainerId = "table-container"
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+const tableContainerId = "table-container";
 
-window.onload = function() {
-  checkCondition();
-  generateTable();
-}
+window.onload = function () {
+  addEmployeeInfoInputs();
+  addCustomerBookingInputs();
+};
 
-function isFieldsFilled() {
-  var num_employees = document.getElementById("num-employees");
-  var num_days = document.getElementById("num-days");
-  var num_hours = document.getElementById("num-hours");
-  return num_employees.value !== "" && num_days.value !== "" && num_hours.value !== "";
-}
-
-// Check if forms basic fields have values first
-function checkCondition() {
-  var conditionalInput = document.querySelectorAll('.conditional');
-  if (isFieldsFilled()) {
-    conditionalInput.forEach(function (element) {
-      element.classList.remove("hidden");
-    });
-    generateTable();
+function addEmployeeInfoInputs() {
+  var num_employees = parseInt(document.getElementById("num-employees").value);
+  var employees_info_list = document.getElementById("employees-info-list");
+  if (num_employees > 0) {
+    employees_info_list.classList.remove("hidden");
+    var currentList = employees_info_list.querySelectorAll("input").length;
+    var diff = num_employees - currentList;
+    if (diff > 0) {
+      for (var i = currentList; i < num_employees; i++) {
+        var li = document.createElement("li");
+        var fieldset = document.createElement("fieldset");
+        var legend = document.createElement("legend");
+        legend.textContent = "Employee " + (i + 1) + " information";
+        var label = document.createElement("label");
+        var input = document.createElement("input");
+        label.setAttribute("for", "employee-name" + i);
+        label.textContent = "Name";
+        input.setAttribute("type", "string");
+        input.setAttribute("id", "employee-name" + i);
+        fieldset.appendChild(legend);
+        fieldset.appendChild(label);
+        fieldset.appendChild(input);
+        li.appendChild(fieldset);
+        employees_info_list.appendChild(li);
+      }
+    } else {
+      var currentList = employees_info_list.querySelectorAll("li");
+      for (var i = currentList.length; i > num_employees; i--) {
+        var last = currentList[i - 1];
+        employees_info_list.removeChild(last);
+      }
+    }
   } else {
-    conditionalInput.forEach(function (element) {
-      element.classList.add("hidden");
-    });
-    destroyTable();
+    employees_info_list.classList.add("hidden");
   }
 }
 
-// When basic fields have values, the table input should be displayed
-function generateTable() {
-    if (!isFieldsFilled()) return;
-
-    var numDays = parseInt(document.getElementById("num-days").value);
-    var numHours = parseInt(document.getElementById("num-hours").value);
-    var tableContainer = document.getElementById(tableContainerId);
-
-    var tableHTML = '<table>';
-
-    for (var i = 0; i < numDays + 1; i++) {
-        tableHTML += '<tr>';
-
-        for (var j = 0; j < numHours + 1; j++) {
-            if (i === 0 && j === 0) {
-                tableHTML += '<td></td>'; // Empty cell at the top-left corner
-            } else if (i === 0) {
-                tableHTML += '<td>' + j + '</td>'; // Column headers
-            } else if (j === 0) {
-                tableHTML += '<td>' + i + '</td>'; // Row headers
-            } else {
-                tableHTML += '<td><input type="number" data-row="' + i + '" data-col="' + j + '"></td>'; // Input fields
-            }
-        }
-
-        tableHTML += '</tr>';
+function addCustomerBookingInputs() {
+  var num_bookings = parseInt(document.getElementById("num-bookings").value);
+  var customer_bookings_list = document.getElementById("customer-bookings-list");
+  if (num_bookings > 0) {
+    customer_bookings_list.classList.remove("hidden");
+    var currentList = customer_bookings_list.querySelectorAll("fieldset").length;
+    var diff = num_bookings - currentList;
+    if (diff > 0) {
+      for (var i = currentList; i < num_bookings; i++) {
+        var li = document.createElement("li");
+        var fieldset = document.createElement("fieldset");
+        var legend = document.createElement("legend");
+        legend.textContent = "Booking " + (i + 1);
+        var labelDay = document.createElement("label");
+        var inputDay = document.createElement("input");
+        labelDay.setAttribute("for", "day-of-booking" + i);
+        labelDay.textContent = "Day";
+        inputDay.setAttribute("type", "number");
+        inputDay.setAttribute("id", "day-of-booking" + i);
+        var labelHour = document.createElement("label");
+        var inputHour = document.createElement("input");
+        labelHour.setAttribute("for", "booking-hour" + i);
+        labelHour.textContent = "Start hour";
+        inputHour.setAttribute("type", "number");
+        inputHour.setAttribute("id", "booking-hour" + i);
+        var labelBookings = document.createElement("label");
+        var inputBookings = document.createElement("input");
+        labelBookings.setAttribute("for", "number-of-bookings" + i);
+        labelBookings.textContent = "Number of bookings on the hour";
+        inputBookings.setAttribute("type", "number");
+        inputBookings.setAttribute("id", "number-of-bookings" + i);
+        fieldset.appendChild(legend);
+        fieldset.appendChild(labelDay);
+        fieldset.appendChild(inputDay);
+        fieldset.appendChild(labelHour);
+        fieldset.appendChild(inputHour);
+        fieldset.appendChild(labelBookings);
+        fieldset.appendChild(inputBookings);
+        li.appendChild(fieldset);
+        customer_bookings_list.appendChild(li);
+      }
+    } else {
+      var currentList = customer_bookings_list.querySelectorAll("li");
+      for (var i = currentList.length; i > num_bookings; i--) {
+        var last = currentList[i - 1];
+        customer_bookings_list.removeChild(last);
+      }
     }
-
-    tableHTML += '</table>';
-
-    tableContainer.innerHTML = tableHTML;
-}
-
-function destroyTable() {
-  var tableContainer = document.getElementById(tableContainerId);
-  tableContainer.innerHTML = "";
+  } else {
+    customer_bookings_list.classList.add("hidden");
+  }
 }
 
 // Handle form submit
@@ -73,99 +102,113 @@ async function handleSubmit(event) {
   event.preventDefault();
 
   // Clear previous results
-  const resultsDiv = document.querySelector('#results');
-  resultsDiv.innerHTML = '';
-
-  // Show loader animation
-  const juhaMietoImg = document.createElement('img');
-  juhaMietoImg.classList.add('mietaa');
-  juhaMietoImg.src = '../mietaa.jpg';
-  juhaMietoImg.alt = 'Juha Mieto';
-  juhaMietoImg.classList.add('loadingImage');
-  resultsDiv.appendChild(juhaMietoImg);
+  const resultsDiv = document.querySelector("#results");
+  resultsDiv.innerHTML = "";
 
   // Get form values
-  const numEmployeesInput = document.querySelector('#num-employees');
-  const numDaysInput = document.querySelector('#num-days');
-  const numHoursInput = document.querySelector('#num-hours');
+  const numEmployeesInput = document.querySelector("#num-employees");
+  const numDaysInput = document.querySelector("#num-days");
+  const numHoursInput = document.querySelector("#num-hours");
   const numEmployees = parseInt(numEmployeesInput.value);
   const numDays = parseInt(numDaysInput.value);
   const numHours = parseInt(numHoursInput.value);
 
-  const customerBookings = [];
-
   // Get customer bookings
-  for (var i = 1; i < numDays + 1; i++) {
-
-    for (var j = 1; j < numHours + 1; j++) {
-      var input = document.querySelector('input[data-row="' + i + '"][data-col="' + j + '"]');
-      if (input.value !== "") {
-        const booking = {
-          day: i-1,
-          hour: j-1,
-          bookings: input.value, 
-        }
-        customerBookings.push(booking);
-      }
+  var customer_bookings_list = document.getElementById("customer-bookings-list");
+  var customer_bookings_inputs = customer_bookings_list.querySelectorAll("input");
+  var customer_booking_input_values = Array.from(customer_bookings_inputs).map((b) => parseInt(b.value));
+  const isSomeBookingInvalid = customer_booking_input_values.some((b) => b < 0)
+  if (isSomeBookingInvalid) {
+    alert("Bookings values must be positive integers");
+    return;
+  }
+  if (customer_booking_input_values.length % 3 !== 0) {
+      alert("Number of bookings must be divisable by three");
+      return;
+  }
+  var customerBookings = [];
+  var i = 0;
+  while (i < customer_booking_input_values.length) {
+    const booking = {
+      day: customer_booking_input_values[i],
+      hour: customer_booking_input_values[i+1],
+      bookings: customer_booking_input_values[i+2], 
     }
+    customerBookings.push(booking);
+    i = i + 3;
   }
 
   // Validate form values
   if (!Number.isInteger(numEmployees) || numEmployees <= 0) {
-    alert('Number of employees must be a positive integer');
+    alert("Number of employees must be a positive integer");
     return;
   }
   if (!Number.isInteger(numDays) || numDays <= 0) {
-    alert('Number of days must be a positive integer');
+    alert("Number of days must be a positive integer");
     return;
   }
   if (!Number.isInteger(numHours) || numHours <= 0) {
-    alert('Number of hours must be a positive integer');
+    alert("Number of hours must be a positive integer");
     return;
   }
 
+
+  // Show loader animation
+  const juhaMietoImg = document.createElement("img");
+  juhaMietoImg.classList.add("mietaa");
+  juhaMietoImg.src = "../mietaa.jpg";
+  juhaMietoImg.alt = "Juha Mieto";
+  juhaMietoImg.classList.add("loadingImage");
+  resultsDiv.appendChild(juhaMietoImg);
+
   // Fetch data
   try {
-    const response = await fetch('http://localhost:5000/endpoint', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ num_employees: numEmployees, num_days: numDays, num_hours: numHours, customer_bookings: customerBookings }),
+    const response = await fetch("http://localhost:5000/endpoint", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        num_employees: numEmployees,
+        num_days: numDays,
+        num_hours: numHours,
+        customer_bookings: customerBookings,
+      }),
     });
     const data = await response.json();
 
-    resultsDiv.innerHTML = '';
+    resultsDiv.innerHTML = "";
 
     // Display days and grids
-    const gridColors = ['gray', 'red', 'blue', 'green', 'yellow', 'purple'];
-    data.res.days.forEach(day => {
-      const dayHeader = document.createElement('h2');
+    const gridColors = ["gray", "red", "blue", "green", "yellow", "purple"];
+    data.res.days.forEach((day) => {
+      const dayHeader = document.createElement("h2");
       dayHeader.textContent = `Day ${day.id}`;
       resultsDiv.appendChild(dayHeader);
 
-      const dayGrid = document.createElement('table');
-      dayGrid.classList.add('grid');
-      const headerRow = document.createElement('tr');
-      const idHeaderCell = document.createElement('th');
-      idHeaderCell.textContent = 'ID';
+      const dayGrid = document.createElement("table");
+      dayGrid.classList.add("grid");
+      const headerRow = document.createElement("tr");
+      const idHeaderCell = document.createElement("th");
+      idHeaderCell.textContent = "ID";
       headerRow.appendChild(idHeaderCell);
       for (let h = 0; h < numHours; h++) {
-        const headerCell = document.createElement('th');
+        const headerCell = document.createElement("th");
         headerCell.textContent = `Hour ${h}`;
         headerRow.appendChild(headerCell);
       }
       dayGrid.appendChild(headerRow);
 
-      day.workers.forEach(worker => {
-        const workerRow = document.createElement('tr');
-        const idCell = document.createElement('td');
+      day.workers.forEach((worker) => {
+        const workerRow = document.createElement("tr");
+        const idCell = document.createElement("td");
         idCell.textContent = `Worker ${worker.id}`;
         workerRow.appendChild(idCell);
         for (let h = 0; h < numHours; h++) {
-          const cell = document.createElement('td');
+          const cell = document.createElement("td");
           if (worker.hours.includes(h)) {
-            cell.style.backgroundColor = gridColors[worker.id % gridColors.length];
+            cell.style.backgroundColor =
+              gridColors[worker.id % gridColors.length];
           } else {
-            cell.style.backgroundColor = 'white';
+            cell.style.backgroundColor = "white";
           }
           workerRow.appendChild(cell);
         }
@@ -178,4 +221,3 @@ async function handleSubmit(event) {
     console.error(error);
   }
 }
-
